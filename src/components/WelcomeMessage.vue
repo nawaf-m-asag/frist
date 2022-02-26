65645071b9c642bbeb6f6edf55f4a18f41d5f45f
Nawaf-m-asag <nm.nawaf2017@gmail.com>
2022-02-26 18:55:45 +0300

    Use Components

---
+++ src/components/WelcomeMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>welcome Message</h1>
+    <h2>This is frist vue app</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "welcomeMessage",
+};
+</script>
+
+<style lang="scss" scoped>
+$fontSize: 50px;
+h1 {
+  font-size: $fontSize;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page 2</h1>
+    <welcomeMessage />
   </div>
 </template>
+
+<script>
+//@ => src  
+import welcomeMessage from "@/components/WelcomeMessage.vue";
+export default {
+  name: "about",
+  components: {
+    welcomeMessage,
+  },
+};
+</script>
