c26d6e335f08771cf0504fe9e8384b298915e476
Nawaf-m-asag <nm.nawaf2017@gmail.com>
2022-02-27 16:45:17 +0300

    AppHeader Router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
-     <router-link to="/help">Help</router-link>
-  </div>
+  <app-header/>
   <router-view />
 </template>
 
+<script>
+import AppHeader from '@/components/AppHeader.vue'
+export default {
+  name:"app",
+  components:{
+    AppHeader
+  }
+}
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="logo">App Logo</div>
+    <div class="header">
+        <div id="nav">
+            <router-link to="/" tag="li" exact><a>Home</a></router-link> |
+            <router-link to="/about"  tag="li"><a>About</a></router-link> |
+            <router-link to="/help"  tag="li" replace><a>Help</a></router-link>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"AppHeader"
+}
+</script>
